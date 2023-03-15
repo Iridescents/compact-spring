26b7fd60d1501cbd6c234e4e22cac6c6ff903953
undertow <1035729652@qq.com>
2023-03-15 08:41:34 +0800

    add PropertyValue for BeanDefinition

--- src/main/java/top/hopestation/springframework/beans/factory/config/BeanDefinition.java
+++ src/main/java/top/hopestation/springframework/beans/factory/config/BeanDefinition.java
@@ -1,11 +1,21 @@
 package top.hopestation.springframework.beans.factory.config;
 
+import top.hopestation.springframework.beans.PropertyValues;
+
 public class BeanDefinition {
 
     private Class beanClass;
 
+    private PropertyValues propertyValues;
+
+
     public BeanDefinition(Class beanClass){
         this.beanClass = beanClass;
+    }
+
+    public BeanDefinition(Class beanClass, PropertyValues propertyValues){
+        this.beanClass = beanClass;
+        this.propertyValues = propertyValues!=null?propertyValues:new PropertyValues();
     }
 
     public Class getBeanClass(){
